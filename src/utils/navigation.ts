0f48d5d130e19bfbbc2e417880113ae27ed3ceeb
MarcRibera <mribera@thesocialcoin.com>
2025-01-08 20:14:08 +0100

    improve footer section

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -6,37 +6,14 @@ const navBarLinks = [
   { name: 'Equipo', url: '#equipo' },
   { name: 'Contacto', url: '#contacto' },
 ];
-// An array of links for footers
-const footerLinks = [
-  {
-    section: 'Ecosystem',
-    links: [
-      { name: 'Documentation', url: '/welcome-to-docs/' },
-      { name: 'Tools & Equipment', url: '/products' },
-      { name: 'Construction Services', url: '/services' },
-    ],
-  },
-  {
-    section: 'Company',
-    links: [
-      { name: 'About us', url: '#' },
-      { name: 'Blog', url: '/blog' },
-      { name: 'Careers', url: '#' },
-      { name: 'Customers', url: '#' },
-    ],
-  },
-];
+
 // An object of links for social icons
 const socialLinks = {
-  facebook: 'https://www.facebook.com/',
-  x: 'https://twitter.com/',
-  github: 'https://github.com/mearashadowfax/ScrewFast',
-  google: 'https://www.google.com/',
-  slack: 'https://slack.com/',
+  // facebook: 'https://www.facebook.com/',
+  insta: 'https://www.instagram.com/fillsderochailopez/',
 };
 
 export default {
   navBarLinks,
-  footerLinks,
   socialLinks,
 };
