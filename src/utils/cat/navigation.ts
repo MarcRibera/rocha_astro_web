a03fa87e50b84faf4a08cd3d86e2c2e415fa54bf
MarcRibera <mribera@thesocialcoin.com>
2025-01-22 00:39:18 +0100

    Translations end

---
+++ src/utils/cat/navigation.ts
@@ -0,0 +1,20 @@
+// An array of links for navigation bar
+const navBarLinks = [
+  { name: 'Home', url: '#home' },
+  { name: 'Serveis', url: '#servicios' },
+  { name: 'Empresa', url: '#empresa' },
+  { name: 'Equip', url: '#equipo' },
+  { name: 'FAQ', url: '#faq' },
+  { name: 'Contacte', url: '#contacto' },
+];
+
+// An object of links for social icons
+const socialLinks = {
+  // facebook: 'https://www.facebook.com/',
+  insta: 'https://www.instagram.com/fillsderochailopez/',
+};
+
+export default {
+  navBarLinks,
+  socialLinks,
+};
